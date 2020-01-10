e04c25a741c901a5860f13daf97903db6f65336c
Outurnate <joseph@outurnate.com>
2020-01-09 20:23:54 -0500

    get rid of vcpkg; add grpc

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git submodule update --init --recursive --progress
-mkdir -p build
-sudo vcpkg install boost-core boost-circular-buffer boost-lockfree libsoundio fftw3 range-v3 spdlog fftwpp fmt cpptoml ogre
-cmake -DCMAKE_CXX_FLAGS="-ggdb" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=/usr/share/vcpkg/scripts/buildsystems/vcpkg.cmake -H. -Bbuild
-cmake --build build
