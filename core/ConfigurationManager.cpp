53e2587269efddfeaad09a52ada5d274cdae6484
Outurnate <joseph@outurnate.com>
2020-01-13 22:13:11 -0500

    this is the wrong way to do this

--- core/ConfigurationManager.cpp
+++ core/ConfigurationManager.cpp
@@ -1,6 +1,55 @@
 #include "ConfigurationManager.hpp"
 
+#include <grpcpp/grpcpp.h>
 #include <fstream>
+#include <type_traits>
+
+template<class T> struct always_false : std::false_type {};
+
+bool ConfigurationManager::Option::validateNewValue(const ConfigurationManager::OptionValue& value) const
+{
+  if (validationCallback)
+    return validationCallback.value()(value);
+
+  auto v = std::visit([&](auto&& arg)
+  {
+    using T = std::decay_t<decltype(arg)>;
+    if constexpr (std::is_same_v<T, int>)
+    {
+      if (this->min && this->max)
+      {
+        const int min = std::get<int>(this->min.value());
+        const int max = std::get<int>(this->max.value());
+        return min < arg && arg < max;
+      }
+      else
+        return true;
+    }
+    else if constexpr (std::is_same_v<T, double>)
+    {
+      if (this->min && this->max)
+      {
+        const double min = std::get<double>(this->min.value());
+        const double max = std::get<double>(this->max.value());
+        return min < arg && arg < max;
+      }
+      else
+        return true;
+    }
+    else if constexpr (std::is_same_v<T, std::string>)
+      return true;
+    else if constexpr (std::is_same_v<T, bool>)
+      return true;
+    else
+      static_assert(always_false<T>::value, "non-exhaustive visitor!");
+  }, value);
+
+  return v;
+}
+
+void ConfigurationManager::Option::notifyUpdated()
+{
+}
 
 void ConfigurationManager::OptionSet::registerOption(ConfigurationManager::Option option)
 {
@@ -15,7 +64,17 @@ std::string locateConfigFile()
 ConfigurationManager::ConfigurationManager(ConfigurationManager::OptionSet options)
   : current(options),
     configFile(cpptoml::parse_file(locateConfigFile())),
-    dirty(false)
+    dirty(false),
+    rpcThread([&]()
+  {
+    std::string serverAddress("0.0.0.0:50051");
+
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(serverAddress, grpc::InsecureServerCredentials());
+    builder.RegisterService(this);
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    server->Wait();
+  })
 {
   for (auto& optionDefinition : current.options)
     std::visit([&optionDefinition, this](auto&& arg)
@@ -48,4 +107,156 @@ void ConfigurationManager::flush()
   configFile = newConfig;
 
   dirty = false;
+}
+
+cfs::SettingType getType(const ConfigurationManager::OptionValue& option)
+{
+  return std::visit([](auto&& arg)
+  {
+    using T = std::decay_t<decltype(arg)>;
+    if constexpr (std::is_same_v<T, int>)
+      return cfs::SettingType::INTEGER;
+    else if constexpr (std::is_same_v<T, double>)
+      return cfs::SettingType::REAL;
+    else if constexpr (std::is_same_v<T, std::string>)
+      return cfs::SettingType::STRING;
+    else if constexpr (std::is_same_v<T, bool>)
+      return cfs::SettingType::BOOL;
+    else
+      static_assert(always_false<T>::value, "non-exhaustive visitor!");
+  }, option);
+}
+
+std::optional<bool> validateValue(const ConfigurationManager::Option& option, const cfs::SettingValue& newValue)
+{
+  if (newValue.value_case() == cfs::SettingValue::VALUE_NOT_SET)
+    return std::nullopt;
+
+  switch(getType(option.value))
+  {
+  case cfs::SettingType::BOOL:
+    if (newValue.value_case() != cfs::SettingValue::kBoolValue)
+      return std::nullopt;
+    else
+      return option.validateNewValue(newValue.boolvalue());
+    break;
+  case cfs::SettingType::INTEGER:
+    if (newValue.value_case() != cfs::SettingValue::kIntValue)
+      return std::nullopt;
+    else
+      return option.validateNewValue(newValue.intvalue());
+    break;
+  case cfs::SettingType::REAL:
+    if (newValue.value_case() != cfs::SettingValue::kRealValue)
+      return std::nullopt;
+    else
+      return option.validateNewValue(newValue.realvalue());
+    break;
+  case cfs::SettingType::STRING:
+    if (newValue.value_case() != cfs::SettingValue::kStringValue)
+      return std::nullopt;
+    else
+      return option.validateNewValue(newValue.stringvalue());
+    break;
+  }
+}
+
+grpc::Status ConfigurationManager::ListSettings(grpc::ServerContext* context, const cfs::Empty* request, grpc::ServerWriter<cfs::SettingDescription>* writer)
+{
+  (void)context;
+  (void)request;
+
+  for (auto& option : current.options)
+  {
+    cfs::SettingDescription description;
+    description.set_name(option.second.name);
+    writer->Write(description);
+  }
+
+  return grpc::Status::OK;
+}
+
+grpc::Status ConfigurationManager::GetDetails(grpc::ServerContext* context, const cfs::SettingDescription* request, cfs::SettingDetails* response)
+{
+  (void)context;
+
+  auto mapValue = current.options.find(request->name());
+  bool doesOptionExist = mapValue != current.options.end();
+
+  if (!doesOptionExist)
+    return grpc::Status::CANCELLED;
+
+  response->set_type(getType(mapValue->second.value));
+
+  return grpc::Status::OK;
+}
+
+grpc::Status ConfigurationManager::ValidateValue(grpc::ServerContext* context, const cfs::SettingValue* request, cfs::Result* response)
+{
+  (void)context;
+
+  auto mapValue = current.options.find(request->name());
+  bool doesOptionExist = mapValue != current.options.end();
+
+  if (!doesOptionExist)
+    return grpc::Status::CANCELLED;
+
+  std::optional<bool> success = validateValue(mapValue->second, *request);
+
+  if (!success)
+    return grpc::Status::CANCELLED;
+  else
+  {
+    response->set_success(success.value());
+    return grpc::Status::OK;
+  }
+}
+
+grpc::Status ConfigurationManager::SetSetting(grpc::ServerContext* context, const cfs::SettingValue* request, cfs::Result* response)
+{
+  (void)context;
+
+  auto mapValue = current.options.find(request->name());
+  bool doesOptionExist = mapValue != current.options.end();
+
+  if (!doesOptionExist)
+    return grpc::Status::CANCELLED;
+}
+
+grpc::Status ConfigurationManager::GetSetting(grpc::ServerContext* context, const cfs::SettingDescription* request, cfs::SettingValue* response)
+{
+  (void)context;
+
+  auto mapValue = current.options.find(request->name());
+  bool doesOptionExist = mapValue != current.options.end();
+
+  if (!doesOptionExist)
+    return grpc::Status::CANCELLED;
+
+  switch(getType(mapValue->second.value))
+  {
+  case cfs::SettingType::BOOL:
+    response->set_boolvalue(std::get<bool>(mapValue->second.value));
+    return grpc::Status::OK;
+
+  case cfs::SettingType::INTEGER:
+    response->set_intvalue(std::get<int>(mapValue->second.value));
+    return grpc::Status::OK;
+
+  case cfs::SettingType::REAL:
+    response->set_realvalue(std::get<double>(mapValue->second.value));
+    return grpc::Status::OK;
+
+  case cfs::SettingType::STRING:
+    response->set_stringvalue(std::get<std::string>(mapValue->second.value));
+    return grpc::Status::OK;
+
+  default:
+    return grpc::Status::CANCELLED;
+  }
+}
+
+grpc::Status ConfigurationManager::ValidValues(grpc::ServerContext* context, const cfs::SettingDescription* request, grpc::ServerWriter<cfs::SettingValue>* writer)
+{
+  (void)context;
 }
--- core/ConfigurationManager.hpp
+++ core/ConfigurationManager.hpp
@@ -4,22 +4,41 @@
 #include <cpptoml.h>
 #include <variant>
 #include <map>
+#include <thread>
+#include <memory>
+#include <functional>
+#include <optional>
+#include <ConfigurationServer.grpc.pb.h>
 
-class ConfigurationManager
+class ConfigurationManager final : public cfs::ConfigurationServer::Service
 {
 public:
-  typedef std::variant<double, std::string, bool> OptionValue;
+  typedef std::variant<double, std::string, bool, int> OptionValue;
 
   struct Option
   {
+    typedef std::function<bool(const OptionValue&)> ValidationCallback;
+    typedef std::function<void(const OptionValue&)> UpdateCallback;
+
     std::string name;
     OptionValue defaultValue;
     OptionValue value;
+    std::optional<OptionValue> min;
+    std::optional<OptionValue> max;
+    std::optional<ValidationCallback> validationCallback;
+    std::optional<UpdateCallback> updateCallback;
 
-    Option(const std::string& name, const OptionValue& defaultValue)
+    Option(std::string name, OptionValue defaultValue, std::optional<UpdateCallback> updateCallback = std::nullopt, std::optional<OptionValue> min = std::nullopt, std::optional<OptionValue> max = std::nullopt, std::optional<ValidationCallback> validationCallback = std::nullopt)
       : name(name),
         defaultValue(defaultValue),
-        value(defaultValue) {}
+        value(defaultValue),
+        updateCallback(updateCallback),
+        min(min),
+        max(max),
+        validationCallback(validationCallback) {}
+
+    bool validateNewValue(const OptionValue& value) const;
+    void notifyUpdated();
   };
 
   struct OptionSet
@@ -52,10 +71,18 @@ public:
     if (flush)
       this->flush();
   }
+
+  grpc::Status ListSettings(grpc::ServerContext* context, const cfs::Empty* request, grpc::ServerWriter<cfs::SettingDescription>* writer) override;
+  grpc::Status GetDetails(grpc::ServerContext* context, const cfs::SettingDescription* request, cfs::SettingDetails* response) override;
+  grpc::Status ValidateValue(grpc::ServerContext* context, const cfs::SettingValue* request, cfs::Result* response) override;
+  grpc::Status SetSetting(grpc::ServerContext* context, const cfs::SettingValue* request, cfs::Result* response) override;
+  grpc::Status GetSetting(grpc::ServerContext* context, const cfs::SettingDescription* request, cfs::SettingValue* response) override;
+  grpc::Status ValidValues(grpc::ServerContext* context, const cfs::SettingDescription* request, grpc::ServerWriter<cfs::SettingValue>* writer) override;
 private:
   OptionSet current;
   std::shared_ptr<cpptoml::table> configFile;
   bool dirty;
+  std::thread rpcThread;
 };
 
 #endif
